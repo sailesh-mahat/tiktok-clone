1663a20961fb05907a1933e09dd7cc30d9e751ad
sailesh-mahat <sailesharsenal@gmail.com>
2020-09-06 23:53:24 +0200

    footer text and ticker added

--- src/Video.js
+++ src/Video.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import "./Video.css";
+import VideoFooter from "./VideoFooter";
 
 function Video() {
+    const [playing, setPlaying] = useState(false);
+    const videoRef = useRef(null);
+
+    const handleVideoPress = () => {
+        if (playing) {
+            videoRef.current.pause();
+            setPlaying(false);
+        } else {
+            videoRef.current.play();
+            setPlaying(true);
+        }
+    };
+
     return (
         <div className="video">
             <video 
+                onClick={handleVideoPress}
                 className="video__player" 
-                loop
-                src="https://youtu.be/NLOp_6uPccQ">
+                loop 
+                ref={videoRef}
+                src="https://player.vimeo.com/video/306969314\">
 
             </video>
+
+            <VideoFooter />
             
             {/* VideoFooter */}
             {/* VideoSidebar */}
@@ -18,4 +36,4 @@ function Video() {
     );
 }
 
-export default Video
+export default Video;
